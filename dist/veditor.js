edfd6e4f96f2689185bb649d458f864c569d8e4c
Lin Jingbin <linjingbin@proudsmart.com>
2018-07-13 17:56:26 +0800

    v1.0.22

--- dist/veditor.js
+++ dist/veditor.js
@@ -95,8 +95,11 @@
       callback && callback(obj[i], i);
     }
   }
+  function createDocumentFragment(){
+    return document.createDocumentFragment()
+  }
   function createElement(tag, id, cls, style){
-    var dom = document.createElement("tag");
+    var dom = document.createElement(tag);
     id && dom.setAttribute("id", id);
     cls && dom.setAttribute("class", cls);
     style && eachProp(cls, function(elem, attr){
@@ -290,6 +293,43 @@
       });
     }
   }
+  function removeChildNode(node){
+    var parentlist = node.parentlist;
+    inx = parentlist.indexOf(node);
+    parentlist.splice(inx, 1);
+  }
+  function pushChildNode(node, child){
+    child.parentlist = node.children;
+    child.parent = node;
+    node.children = node.children || [];
+    node.children.push(child);
+    recursive(node);
+  }
+  function insertChildNode(node, child){
+    var parent = child.parent = node.parent,
+      parentlist = child.parentlist = node.parentlist;
+    inx = parentlist.indexOf(node);
+    parentlist.splice(inx, 0, child);
+    recursive(parent || parentlist);
+  }
+  function afterChildNode(node, child){
+    var parent = child.parent = node.parent,
+      parentlist = child.parentlist = node.parentlist;
+    inx = parentlist.indexOf(node);
+    parentlist.splice(inx + 1, 0, child);
+    recursive(parent || parentlist);
+  }
+  function SessionStorageFactory(){
+    return {
+      get : function(name){
+        return JSON.parse(sessionStorage.getItem(name))
+      },
+      set : function(name, val){
+        sessionStorage.setItem(name, JSON.stringify(val))
+      }
+    }
+  }
+  var storage = SessionStorageFactory();
   /** DragAndDrop */
   var freeboardcontainer = {
     inserted : function(el, b, o, n){
@@ -306,16 +346,16 @@
           toValue,
           appendValue,
           parent,
-          parentNode;
-        body.appendChild(helper);
-        setStyle(helper, {
-          top : (e.pageY - OFFSETY) + "px",
-          left : (e.pageX - OFFSETX) + "px"
-        });
-        if(hasClass(target, "header")){
+          parentNode,
+          fromValue = prop(target, "_header");
+        if(fromValue){
+          body.appendChild(helper);
+          setStyle(helper, {
+            top : (e.pageY - OFFSETY) + "px",
+            left : (e.pageX - OFFSETX) + "px"
+          });
           parent = target.parentNode.parentNode;
           addClass(parent, "hide");
-          fromValue = prop(target, "_header");
           body.onmouseover = mouseover
           body.onmouseout = mouseout;
           body.onmouseup = mouseup;
@@ -329,6 +369,7 @@
           emit("freeboard:change");
         }
         function mouseup(e){
+          console.error("body mouseup");
           var inx, cl,
             target = (function(t){
               return findParent(t, function(n){
@@ -354,7 +395,7 @@
           }
           body.onmouseover = null
           body.onmouseout = null;
-          body.mouseup = null;
+          body.onmouseup = null;
           body.onmousemove = null;
         }
       }
@@ -380,26 +421,6 @@
       }
     }
   }
-  function removeChildNode(node){
-    var parentlist = node.parentlist;
-    inx = parentlist.indexOf(node);
-    parentlist.splice(inx, 1);
-  }
-  function pushChildNode(node, child){
-    child.parentlist = node.children;
-    child.parent = node;
-    node.children = node.children || [];
-    node.children.push(child);
-    recursive(node);
-  }
-  function insertChildNode(node, child){
-    var parent = child.parent = node.parent,
-      parentlist = child.parentlist = node.parentlist;
-    inx = parentlist.indexOf(node);
-    parentlist.splice(inx, 0, child);
-    recursive(parent || parentlist);
-  }
-
   var tools = {
     inserted : function(el, b, o, n){
       var vm = o.context;
@@ -412,22 +433,24 @@
           toValue,
           parent,
           parentNode;
-        body.appendChild(helper);
-        setStyle(helper, {
-          top : (e.pageY - OFFSETY) + "px",
-          left : (e.pageX - OFFSETX) + "px"
-        });
-        attachEvent(body, "mousemove", function(e){
-          e.preventDefault();
+        if(fromValue){
+          body.appendChild(helper);
           setStyle(helper, {
             top : (e.pageY - OFFSETY) + "px",
-            left :  (e.pageX - OFFSETX) + "px"
+            left : (e.pageX - OFFSETX) + "px"
           });
-        })
-        body.onmouseover = mouseover
-        body.onmouseout = mouseout;
-        body.onmouseup = mouseup;
-        emit("freeboard:change");
+          attachEvent(body, "mousemove", function(e){
+            e.preventDefault();
+            setStyle(helper, {
+              top : (e.pageY - OFFSETY) + "px",
+              left :  (e.pageX - OFFSETX) + "px"
+            });
+          })
+          body.onmouseover = mouseover
+          body.onmouseout = mouseout;
+          body.onmouseup = mouseup;
+          emit("freeboard:change");
+        }
         function mouseup(e){
           var inx, cl,
             parentFromValue,
@@ -477,6 +500,136 @@
       }
     }
   }
+  var widget = {
+    inserted : function(el, b){
+      var buttonWrap, widget, target, buttons = [{
+        name : "编辑",
+        onclick : function(e){
+          var val = prop(el.parentNode, "_header");
+        }
+      },{
+        name : "剪切",
+        onclick : function(e){
+          var val = prop(el.parentNode, "_header");
+          storage.set("freeboardcopy", plainClone(val));
+          removeChildNode(val);
+        }
+      },{
+        name : "复制",
+        onclick : function(e){
+          var val = prop(el.parentNode, "_header");
+          storage.set("freeboardcopy", plainClone(val));
+        }
+      },{
+        name : "前插粘贴",
+        onclick : function(e){
+          var val = prop(el.parentNode, "_header"),
+            fromValue = storage.get("freeboardcopy");
+          insertChildNode(val, fromValue);
+        }
+      },{
+        name : "后插粘贴",
+        onclick : function(e){
+          var val = prop(el.parentNode, "_header"),
+            fromValue = storage.get("freeboardcopy");
+          afterChildNode(val, fromValue);
+        }
+      },{
+        name : "删除",
+        onclick : function(e){
+          var val = prop(el.parentNode, "_header");
+          removeChildNode(val);
+        }
+      }];
+      function remove(){
+        widget.remove();
+        buttonWrap = null;
+        widget = null;
+      }
+      el.onclick = function(e){
+        buttonWrap = createElement("div", null, "button-wrap", null),
+          widget = createElement("div", null, "widget-bg", null),
+          fragment = createDocumentFragment();
+          target = e.target;
+        fragment.appendChild(widget);
+        widget.appendChild(buttonWrap);
+        each(buttons, function(btn, i){
+          var node = createElement("div", null, "button-item", null);
+          node.innerText = btn.name;
+          node.onclick = function(e){
+            remove();
+            btn.onclick(e);
+          };
+          buttonWrap.append(node);
+        })
+        document.body.appendChild(fragment);
+        setStyle(buttonWrap, {
+          top : (e.clientY - 10) + "px",
+          left : (e.clientX - 90) + "px"
+        });
+        setTimeout(function(){
+          addClass(widget, "fade-in");
+          addClass(buttonWrap, "fade-in");
+        })
+        widget.onclick = function(e){
+          if(e.eventPhase === 2){
+            remove();
+          }
+        }
+      }
+    }
+  }
+  var blankwidget = {
+    inserted : function(el, b){
+      var buttonWrap, widget, target, buttons = [{
+        name : "粘贴",
+        onclick : function(e){
+          var appendValue = prop(el, "_append"),
+            fromValue = storage.get("freeboardcopy")
+          pushChildNode(appendValue, fromValue);
+        }
+      }];
+      var value = b.value;
+      function remove(){
+        widget.remove();
+        buttonWrap = null;
+        widget = null;
+      }
+      el.onclick = function(e){
+        if(hasClass(e.target,"end")){
+          buttonWrap = createElement("div", null, "button-wrap", null),
+            widget = createElement("div", null, "widget-bg", null),
+            fragment = createDocumentFragment();
+          target = e.target;
+          fragment.appendChild(widget);
+          widget.appendChild(buttonWrap);
+          each(buttons, function(btn, i){
+            var node = createElement("div", null, "button-item", null);
+            node.innerText = btn.name;
+            node.onclick = function(e){
+              remove();
+              btn.onclick(e);
+            };
+            buttonWrap.append(node);
+          })
+          document.body.appendChild(fragment);
+          setStyle(buttonWrap, {
+            top : (e.clientY - 10) + "px",
+            left : (e.clientX - 90) + "px"
+          });
+          setTimeout(function(){
+            addClass(widget, "fade-in");
+            addClass(buttonWrap, "fade-in");
+          })
+          widget.onclick = function(e){
+            if(e.eventPhase === 2){
+              remove();
+            }
+          }
+        }
+      }
+    }
+  }
   var setvalue = {
     inserted : function(el, b){
       var arg = b.arg;
@@ -498,7 +651,9 @@
             <div class=\"blank\" v-setvalue:blank=\"option\"></div>\
             <div class=\"col-xs-12 drdp\"\
               v-bind:class=\"cls\">\
-              <div class=\"header\" v-setvalue:header=\"option\"></div>\
+              <div class=\"header\" v-setvalue:header=\"option\">\
+                <div class=\"wiget\" v-widget></div>\
+              </div>\
               <div class=\"row item\">\
                 <html-recursive v-for=\"op in option.children\"\
                   v-bind:option=\"op\"\
@@ -512,15 +667,8 @@
             cls : ""
           }
         },
-        methods : {
-          select : function(e){
-            this.cls = "highlight";
-          },
-          unselect : function(e){
-            this.cls = "";
-          }
-        },
         directives : {
+          widget : widget,
           setvalue : setvalue
         },
         props : ['option']
@@ -533,6 +681,7 @@
               ></html-recursive>\
             <div class=\"blank\" \
               v-bind:class=\"end\" \
+              v-blankwidget:isend=\"end\"\
               v-setvalue:append=\"option\">\
             </div>\
           </div>",
@@ -547,13 +696,13 @@
             var vm = this;
             var _END = "end"
             var option = vm.option;
-            if(vm.empty == true) return _END;
             if(!option.children) return _END;
             if(option.children.length == 0) return _END;
             return "";
           }
         },
         directives : {
+          blankwidget : blankwidget,
           setvalue : setvalue
         },
         props : ['option']
@@ -563,7 +712,9 @@
           <div class=\"blank\" v-setvalue:blank=\"option\"></div>\
             <div class=\"drdp\"\
               v-bind:class=\"cls\">\
-              <div class=\"header\" v-setvalue:header=\"option\"></div>\
+              <div class=\"header\" v-setvalue:header=\"option\">\
+                <div class=\"wiget\" v-widget></div>\
+              </div>\
               <component v-bind:is=\"'fb-' + option.type\" v-bind:value=\"option.id\"></component>\
             </div>\
           </div>",
@@ -572,15 +723,8 @@
             cls : ""
           }
         },
-        methods : {
-          select : function(e){
-            this.cls = "highlight";
-          },
-          unselect : function(e){
-            this.cls = "";
-          }
-        },
         directives : {
+          widget : widget,
           setvalue : setvalue
         },
         props : ['option']
@@ -593,7 +737,7 @@
             if(type != "row" && type != "col") return "tool"
             return "fb" + type
           })(type)
-          return tp;
+        return tp;
       }
     },
     props : ["option"]
@@ -663,14 +807,6 @@
             cls : ""
           }
         },
-        methods : {
-          select : function(e){
-            this.cls = "highlight";
-          },
-          unselect : function(e){
-            this.cls = "";
-          }
-        },
         directives : {
           setvalue : setvalue
         },
@@ -738,25 +874,186 @@
       }
     }
   }
+  var columnEditor = {
+    template : "<div class='col-editor'>\
+      <div class=\"row\">\
+        <div class=\"col-xs-9\">\
+          <div class=\"row\">\
+            <div v-for=\"d in colData.children\"\
+              class=\"col-prev\"\
+              v-bind:class=\"d.class\">\
+                {{d.colnum}}\
+            </div>\
+          </div>\
+          <div class=\"rulerwrap\">\
+            <div v-select:status='i'\
+              draggable=\"false\"\
+              v-setvalue:status='i'\
+              v-for=\"i in options\"\
+              v-bind:class='column(i)'\
+              class=\"column\"\
+            ></div>\
+          </div>\
+        </div>\
+        <div class=\"col-xs-3\">\
+          <div v-setvalue:header=\"colData\" class=\"drag\">拖拽</div>\
+        </div>\
+      </div>\
+    </div>",
+    data : function(){
+      var arr = [];
+      for(var i = 0; i < 11; i++){
+        arr.push({
+          value : false
+        });
+      }
+      return {
+        options : arr
+      }
+    },
+    props : ["colData"],
+    methods : {
+      column : function(active){
+        return active.value ? "active" : "";
+      }
+    },
+    directives : {
+      setvalue : setvalue,
+      select: {
+        inserted: function (el, b, o, n) {
+          var body = document.body,
+            vm = o.context,
+            cacheClick,
+            value = b.value;
+          el.onmousedown = mousedown;
+          function redraw(){
+            vm.colData.children = (function(options){
+              var rs = [], inx = 0;
+              for(var i in options){
+                if(options[i].value){
+                  rs.push({
+                    type : "col",
+                    "class" : "col-xs-" + (i - 0 + 1 - inx),
+                    "colnum" : (i - 0 + 1 - inx),
+                    children : []
+                  });
+                  inx = i - 0 + 1;
+                }
+              }
+              if(inx < 12){
+                rs.push({
+                  type : "col",
+                  "class" : "col-xs-" + (12 - inx),
+                  "colnum" : (12 - inx),
+                  children : []
+                })
+              }
+              return rs;
+            })(vm.options)
+          }
+          function mouseover(e){
+            var status = prop(e.target, "_status");
+            if(status){
+              cacheClick.value = cacheClick._value;
+              cacheClick = status;
+              cacheClick._value = status.value;
+              status.value = true;
+              redraw();
+            }
+          }
+          function mousedown(e) {
+            var status = prop(e.target, "_status");
+            addClass(el.parentNode, "moved");
+            cacheClick = status;
+            body.onmouseover = mouseover;
+            if(status.value){
+              status._value = false;
+              el.onmouseout = mouseout;
+              body.onmouseup = mouseupcancel;
+            } else {
+              status.value = true;
+              body.onmouseup = mouseup;
+            }
+            //cacheClick._value = status.value;
+            redraw();
+          }
+          function mouseout(e) {
+            var target = (function(t){
+              return findParent(t, function(p){
+                return p === el;
+              })
+            })(e.target)
+            body.onmouseup = mouseup;
+          }
+          function mouseupcancel(e){
+            var status = prop(e.target, "_status");
+            removeClass(el.parentNode, "moved");
+            delete cacheClick._value;
+            cacheClick = null;
+            status.value = false;
+            body.onmouseup = null;
+            body.onmouseover = null;
+            redraw();
+          }
+          function mouseup(e) {
+            var status = prop(e.target, "_status");
+            removeClass(el.parentNode, "moved");
+            status.value = true;
+            body.onmouseup = null;
+            body.onmouseover = null;
+            cacheClick = null;
+            redraw();
+          }
+        }
+      }
+    }
+  }
   var freeboardcontrol = {
     name : "free-board-control",
     template : "\
-      <div class=\"free-board-prev\" v-tools >\
-        <div class=\"comp-tool\"\
-          v-for=\"op in options\"\
-          v-setvalue:header=\"op\"\
-        >\
+      <div class=\"free-board-control\" v-tools >\
+        <column-editor v-setvalue=\"colData\" :colData=\"colData\">\
+        </column-editor>\
+          <div class=\"comp-tool\"\
+            v-for=\"op in options\"\
+            >\
+          <span>{{op.title}}</span>\
+          <div v-setvalue:header=\"op.data\" class='drag'>拖拽</div>\
         </div>\
       </div>",
+    components : {
+      "column-editor" : columnEditor
+    },
     directives : {
       'tools' : tools,
       'setvalue' : setvalue
+    },
+    data : function(){
+      var arr = [];
+      for(var i = 0; i < 11; i++){
+        arr.push(false);
+      }
+      return {
+        split : arr
+      }
+    },
+    data : function(){
+      return {
+        colData : {
+          type : "row",
+          children : [{
+            type : "col",
+            colnum : 12,
+            class: "col-xs-12",
+            children : []
+          }]
+        }
+      }
     },
     computed : {
       options : function(){
         var attr = this.attr("options"),
           options = this.parent().data(attr);
-        //recursive(options, function(n){});
         return options;
       }
     }
